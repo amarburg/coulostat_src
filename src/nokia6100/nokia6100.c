b35e1439cb8546f0b31971be0e9fa205a6ad6866
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2010-12-08 12:25:58 +1300

    First starting to set up LCD code.

--- src/nokia6100/nokia6100.c
+++ src/nokia6100/nokia6100.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  * The MIT License
  *
- * Copyright (c) 2010 Perry Hung.
+ * Copyright (c) 2010 Aaron Marburg.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,12 +31,9 @@
 #include "nvic.h"
 #include "usart.h"
 
-#define USART1_BASE    0x40013800
-#define USART2_BASE    0x40004400
-#define USART3_BASE    0x40004800
-#define UART4_BASE     0x40004C00  // High-density devices only (Maple Native)
-#define UART5_BASE     0x40005000  // High-density devices only (Maple Native)
+#include "nokia6100.h"
 
+/* These defines are redundant with libmaple/usart.c */
 #define USART_UE       BIT(13)
 #define USART_M        BIT(12)
 #define USART_TE       BIT(3)
@@ -44,173 +41,62 @@
 #define USART_RXNEIE   BIT(5)  // read data register not empty interrupt enable
 #define USART_TC       BIT(6)
 
-/* usart descriptor table  */
-struct usart_dev usart_dev_table[] = {
-    [USART1] = {
-        .base = (usart_port*)USART1_BASE,
-        .rcc_dev_num = RCC_USART1,
-        .nvic_dev_num = NVIC_USART1
-    },
-    [USART2] = {
-        .base = (usart_port*)USART2_BASE,
-        .rcc_dev_num = RCC_USART2,
-        .nvic_dev_num = NVIC_USART2
-    },
-    [USART3] = {
-        .base = (usart_port*)USART3_BASE,
-        .rcc_dev_num = RCC_USART3,
-        .nvic_dev_num = NVIC_USART3
-    },
-    /*
-      #if NR_USART >= 5
-      [UART4] = {
-      .base = (usart_port*)UART4_BASE,
-      .rcc_dev_num = RCC_UART4,
-      .nvic_dev_num = NVIC_UART4
-      },
-      [UART5] = {
-      .base = (usart_port*)UART5_BASE,
-      .rcc_dev_num = RCC_UART5,
-      .nvic_dev_num = NVIC_UART5
-      },
-      #endif
-    */
-};
-
-/* usart interrupt handlers  */
-void USART1_IRQHandler(void) {
-    rb_insert(&(usart_dev_table[USART1].rb),
-              (uint8)(((usart_port*)(USART1_BASE))->DR));
-}
-
-void USART2_IRQHandler(void) {
-    rb_insert(&(usart_dev_table[USART2].rb),
-              (uint8)(((usart_port*)(USART2_BASE))->DR));
-}
-
-void USART3_IRQHandler(void) {
-    rb_insert(&usart_dev_table[USART3].rb,
-              (uint8)(((usart_port*)(USART3_BASE))->DR));
-}
-#if NR_USART >= 5
-void UART4_IRQHandler(void) {
-    rb_insert(&usart_dev_table[UART4].rb,
-              (uint8)(((usart_port*)(UART4_BASE))->DR));
-}
-void UART5_IRQHandler(void) {
-    rb_insert(&usart_dev_table[UART5].rb,
-              (uint8)(((usart_port*)(UART5_BASE))->DR));
-}
-#endif
-
 /**
- *  @brief Enable a USART in single buffer transmission mode, multibuffer
- *     receiver mode.
- *  @param usart_num USART to be initialized
- *  @param baud Baud rate to be set at
+ * @brief Initialize USART2 for Nokia communications 
  */
-void usart_init(uint8 usart_num, uint32 baud) {
+void nokia_init( void )
+{
+    uint8_t usart_num = USART2;
+
+    /* Code based loosely on libmaple's usart.[ch] module.  Makes use of some of 
+     * the device definition structures defined there.
+     */
     ASSERT(usart_num <= NR_USART);
     usart_port *port;
-    ring_buffer *ring_buf;
-
-    uint32 clk_speed;
-    uint32 integer_part;
-    uint32 fractional_part;
-    uint32 tmp;
+    usart_pins *pins;
 
     port = usart_dev_table[usart_num].base;
+    pins = usart_dev_table[usart_num].pins;
     rcc_clk_enable(usart_dev_table[usart_num].rcc_dev_num);
     nvic_irq_enable(usart_dev_table[usart_num].nvic_dev_num);
 
     /* usart1 is mad fast  */
     clk_speed = (usart_num == USART1) ? 72000000UL : 36000000UL;
+    
+    /* Configure the pins appropriately  */
+    gpio_set_mode(pins->gpio_port, pins->tx_pin, GPIO_MODE_AF_OUTPUT_PP);
+    gpio_set_mode(pins->gpio_port, pins->rx_pin, GPIO_MODE_INPUT_FLOATING);
+    gpio_set_mode(pins->gpio_port, pins->ck_pin, GPIO_MODE_OUTPUT_PP);
 
-    /* Initialize rx ring buffer  */
-    rb_init(&usart_dev_table[usart_num].rb,
-            sizeof (usart_dev_table[usart_num].rx_buf),
-            usart_dev_table[usart_num].rx_buf);
+    gpio_set_mode( NOKIA_GPIO, NOKIA_CS, GPIO_MODE_OUTPUT_PP );
+    gpio_set_mode( NOKIA_GPIO, NOKIA_RESET, GPIO_MODE_OUTPUT_PP );
 
-    /* Set baud rate  */
-    integer_part = ((25 * clk_speed) / (4 * baud));
-    tmp = (integer_part / 100) << 4;
-
-    fractional_part = integer_part - (100 * (tmp >> 4));
-    tmp |= (((fractional_part * 16) + 50) / 100) & ((uint8)0x0F);
-
-    port->BRR = (uint16)tmp;
-
-    port->CR1 = USART_TE          |    // transmitter enable
-                USART_RE          |    // receiver enable
-                USART_RXNEIE;          // receive interrupt enable
-
-
-    /* Enable the USART and set mode to 8n1 */
-    port->CR1 |= USART_UE;
-}
-
-/**
- *  @brief Turn off all USARTs.
- */
-void usart_disable_all() {
-    usart_disable(USART1);
-    usart_disable(USART2);
-    usart_disable(USART3);
-#if NR_USART >= 5
-    usart_disable(UART4);
-    usart_disable(UART5);
-#endif
-}
-
-/**
- * @brief Turn off a USART.
- * @param USART to be disabled
- */
-void usart_disable(uint8 usart_num) {
-    usart_port *port = usart_dev_table[usart_num].base;
-
-    /* TC bit must be high before disabling the usart  */
-    while((port->CR1 & USART_UE) && !(port->SR & USART_TC))
-        ;
-
-    /* Disable UE */
-    port->CR1 = 0;
-
-    /* Clean up buffer */
-    usart_reset_rx(usart_num);
-}
-
-
-/**
- * @brief Print a null terminated string to the specified USART
- *
- * @param usart_num usart to send on
- * @param str string to send
- */
-void usart_putstr(uint8 usart_num, const char* str) {
-    char ch;
-
-    while((ch = *(str++)) != '\0') {
-        usart_putc(usart_num, ch);
+    if ((usart_num == USART1) ||
+        (usart_num == USART2)) {
+      /* turn off any pwm if there's a conflict on this usart */
+      timer_set_mode(pin->timer_num, pin->compare_num, TIMER_DISABLED);
     }
+
+    /* set clock rate to 6MHz */
+    port->BRR = 36000000L/6000000L; //APB1 bus max speed is 36MHz
+    port->CR1 |= (USART_RE | USART_TE | USART_M); // RX, TX enable, 9-bit
+    port->CR2 |= USART2_CR2_CLKEN; //enable SCK pin CPOL and CPHA are also in this register
+
+
+
+    port->CR1 |= USART_UE; // USART enable
 }
 
-/**
- * @brief Print an unsigned integer to the specified usart
- *
- * @param usart_num usart to send on
- * @param val number to print
+/** 
+ * @brief Reset the LCD screen
  */
-void usart_putudec(uint8 usart_num, uint32 val) {
-    char digits[12];
-    int i;
+void nokia_reset( void ) {
+  uint8_t usart_num = USART2;
+  gpio_write_bit( NOKIA_GPIO, NOKIA_RESET, 0 );
+  delay( 20000 );
+  gpio_write_bit( NOKIA_GPIO, NOKIA_RESET, 1 );
+  delay( 20000 );
 
-    i = 0;
-    do {
-        digits[i++] = val % 10 + '0';
-        val /= 10;
-    } while (val > 0);
-    while (--i >= 0) {
-        usart_putc(usart_num, digits[i]);
-    }
 }
+
+
--- src/nokia6100/nokia6100.h
+++ src/nokia6100/nokia6100.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  * The MIT License
  *
- * Copyright (c) 2010 Perry Hung.
+ * Copyright (c) 2010 Aaron Marburg
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,98 +23,44 @@
  *****************************************************************************/
 
 /**
- * @file usart.h
- * @brief USART definitions and prototypes
+ * @file nokia6100.h
+ * @brief Definitions and prototypes for accessing NOKIA6100 using USART2
+ *
+ * The Nokia 6100 (knockoff) LCD screens require 9-bit SPI.  The SPI block 
+ * on the STM32 can't do 9-bit SPI, but the USART can.  This code
+ * assume USART2.
+ *
+ * The board I'm using has the EPSON S1G15D00 controller, so this code
+ * is written for that module.
  */
 
-#ifndef _USART_H_
-#define _USART_H_
+#ifndef _NOKIA_6100_H_
+#define _NOKIA_6100_H_
 
-#include "ring_buffer.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-#define USART_TXE           BIT(7)
+#define NOKIA_GPIO          GPIOC_BASE
 
-/* usart device numbers  */
-enum {
-    USART1,
-    USART2,
-    USART3,
-    UART4,
-    UART5,
-};
+/* LCD reset (active low) is on PC7 */
+#define NOKIA_RESET         BIT(7)
 
-/* peripheral register struct  */
-typedef struct usart_port {
-    volatile uint32 SR;       // Status register
-    volatile uint32 DR;       // Data register
-    volatile uint32 BRR;      // Baud rate register
-    volatile uint32 CR1;      // Control register 1
-    volatile uint32 CR2;      // Control register 2
-    volatile uint32 CR3;      // Control register 3
-    volatile uint32 GTPR;     // Guard time and prescaler register
-} usart_port;
+/* LCD chip select (active low) is on PC8 */
+#define NOKIA_CS            BIT(8)
 
-/* usart descriptor  */
-struct usart_dev {
-    usart_port *base;
-    ring_buffer rb;
-    uint8 rx_buf[64];
-    const uint8 rcc_dev_num;
-    const uint8 nvic_dev_num;
-};
-
-extern struct usart_dev usart_dev_table[];
 
 /**
- * @brief send one character on a usart
- * @param usart_num usart to send on
- * @param byte byte to send
+ * @brief Initialize USART2 for Nokia communications 
  */
-static inline void usart_putc(uint8 usart_num, uint8 byte) {
-    usart_port *port = usart_dev_table[usart_num].base;
+void nokia_init( void );
 
-    port->DR = byte;
 
-    /* Wait for transmission to complete  */
-    while ((port->SR & USART_TXE) == 0)
-        ;
-}
-
-/**
- * @brief read one character from a usart
- * @param usart_num usart to read from
- * @return byte read
+/** 
+ * @brief Reset the LCD screen
  */
-static inline uint8 usart_getc(uint8 usart_num) {
-    return rb_remove(&usart_dev_table[usart_num].rb);
-}
-
-/**
- * @brief return the amount of data available in the rx buffer
- * @param usart_num which usart to check
- * @return number of bytes in the rx buffer
- */
-static inline uint32 usart_data_available(uint8 usart_num) {
-    return rb_full_count(&usart_dev_table[usart_num].rb);
-}
-
-/**
- * @brief removes the contents of the rx fifo
- * @param usart_num which usart to reset
- */
-static inline void usart_reset_rx(uint8 usart_num) {
-    rb_reset(&usart_dev_table[usart_num].rb);
-}
-
-void usart_init(uint8 usart_num, uint32 baud);
-void usart_disable(uint8 usart_num);
-void usart_disable_all();
-void usart_putstr(uint8 usart_num, const char*);
-void usart_putudec(uint8 usart_num, uint32 val);
+void nokia_reset( void );
 
 #ifdef __cplusplus
 } // extern "C"
