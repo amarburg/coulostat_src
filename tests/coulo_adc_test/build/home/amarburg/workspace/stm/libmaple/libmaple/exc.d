a745b0656e8d1443afbd80c8ff05191f0d00f6d2
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-01-19 12:22:16 +1300

    Remove a build directory inadvertantly added to git and add a few gitignores to tests/ programs.

--- tests/coulo_adc_test/build/home/amarburg/workspace/stm/libmaple/libmaple/exc.d
+++
@@ -1,2 +0,0 @@
-build//home/amarburg/workspace/stm/libmaple/libmaple/exc.o: \
- /home/amarburg/workspace/stm/libmaple/libmaple/exc.S
--- tests/coulo_adc_test/build/home/amarburg/workspace/stm/libmaple/wirish/wirish_math.d
+++
@@ -1,2 +0,0 @@
-build//home/amarburg/workspace/stm/libmaple/wirish/wirish_math.o: \
- /home/amarburg/workspace/stm/libmaple/wirish/wirish_math.cpp
