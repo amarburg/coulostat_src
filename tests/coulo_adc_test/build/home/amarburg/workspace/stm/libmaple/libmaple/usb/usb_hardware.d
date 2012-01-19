f4908822817baadaa87dbc6ac2ed91cc906a0edb
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-01-19 15:46:18 +1300

    Removed a few builds from the git tree.

--- tests/coulo_adc_test/build/home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_hardware.d
+++
@@ -1,8 +0,0 @@
-build//home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_hardware.o: \
- /home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_hardware.c \
- /home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_hardware.h \
- /home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_lib/usb_type.h
-
-/home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_hardware.h:
-
-/home/amarburg/workspace/stm/libmaple/libmaple/usb/usb_lib/usb_type.h:
--- tests/spi_test/build/home/amarburg/workspace/stm/libmaple/wirish/cxxabi-compat.d
+++
@@ -1,2 +0,0 @@
-build//home/amarburg/workspace/stm/libmaple/wirish/cxxabi-compat.o: \
- /home/amarburg/workspace/stm/libmaple/wirish/cxxabi-compat.cpp
